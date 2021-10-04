a9c44409f8fd494a0ea1859ce3efbeebe8cd2bfe
demidraksin <greatraksin@icloud.com>
2021-10-04 16:26:56 +0300

    Adding lab_2 tasks

---
+++ 02_lessson_0410/02_lab_task_1_S.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+t = Turtle()
+
+t.shape('turtle')
+
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+
+done()
---
+++ 02_lessson_0410/04_triangle.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+t = Turtle()
+t.shape('turtle')
+
+# TODO: нарисовать треугольник
+for i in range(3):
+    t.fd(100)
+    t.lt(120)
+
+done()
